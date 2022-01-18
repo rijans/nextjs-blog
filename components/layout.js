406e0267443547dd202396290a6a822f61dd376e
Jaber Al Nahian <jaber@Jabers-MacBook-Pro.local>
2022-01-18 12:15:15 +0600

    Fixes

--- components/layout.js
+++ components/layout.js
@@ -7,11 +7,13 @@ import Link from 'next/link'
 const name = 'Jaber Al Nahian'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({children, home}) {
     return (
         <div className={styles.container}>
             <Head>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.ico"/>
+                <title>{siteTitle} || Jaber's NextJS Demo</title>
+                <meta name="title" content={siteTitle}/>
                 <meta
                     name="description"
                     content="Learn how to build a personal website using Next.js"
@@ -22,8 +24,9 @@ export default function Layout({ children, home }) {
                         siteTitle
                     )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
                 />
-                <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
+
+                <meta name="og:title" content={siteTitle}/>
+                <meta name="twitter:card" content="summary_large_image"/>
             </Head>
             <header className={styles.header}>
                 {home ? (
